24d8a39afa8a6e9b6927b1a612870d3dad598c8e
LEGION-DMC <jonathanrand.zxz@gmail.com>
2025-03-23 14:21:37 +0800

    app.js

--- app.js
+++ app.js
@@ -191,20 +191,7 @@ function showModal(movie) {
         `;
     }
 
-	function initMobileMenu() {
-    if (window.innerWidth < 769) {
-        const menuButton = document.querySelector('.menu-button');
-        const filtersWrapper = document.querySelector('.filters-wrapper');
-        
-        menuButton.addEventListener('click', () => {
-            filtersWrapper.classList.toggle('active');
-        });
-        
-        document.addEventListener('click', (e) => {
-            if (!e.target.closest('.filters-wrapper') && 
-                !e.target.closest('.menu-button')) {
-                filtersWrapper.classList.remove('active');
-            }
-        });
-    }
+    html += '</div>';
+    modalContent.innerHTML = html;
+    modalOverlay.style.display = 'flex';
 }
